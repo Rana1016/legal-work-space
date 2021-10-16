914e516f992cddc855c6354411b59be9516cf040
MZakria3637 <zakriamuhammad3637@gmail.com>
2021-10-16 12:02:14 +0500

    conflicts resolved

--- src/app/cases/components/case-details/case-details.module.ts
+++ src/app/cases/components/case-details/case-details.module.ts
@@ -3,8 +3,6 @@ import { CommonModule } from '@angular/common';
 
 import { CaseDetailsRoutingModule } from './case-details-routing.module';
 import { CaseDetailsComponent } from './case-details.component';
-<<<<<<< HEAD
-=======
 import { ProfileComponent } from './children/profile/profile.component';
 import { DetailsComponent } from './children/details/details.component';
 import { AccountsComponent } from './children/accounts/accounts.component';
@@ -16,14 +14,10 @@ import { DeadlinesComponent } from './children/deadlines/deadlines.component';
 import { WorkflowComponent } from './children/workflow/workflow.component';
 import { ActivitiesComponent } from './children/activities/activities.component';
 import { StatusComponent } from './children/status/status.component';
->>>>>>> b0f387b62533fb66220d6ee325ec86e63ee463c4
 
 
 @NgModule({
   declarations: [
-<<<<<<< HEAD
-    CaseDetailsComponent
-=======
     CaseDetailsComponent,
     ProfileComponent,
     DetailsComponent,
@@ -36,7 +30,6 @@ import { StatusComponent } from './children/status/status.component';
     WorkflowComponent,
     ActivitiesComponent,
     StatusComponent
->>>>>>> b0f387b62533fb66220d6ee325ec86e63ee463c4
   ],
   imports: [
     CommonModule,

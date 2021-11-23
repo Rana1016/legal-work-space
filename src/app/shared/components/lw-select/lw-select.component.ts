6100bde09a67b7e48846d2bdbb8cec727ec49e37
Sikandaraliakram <Sikandaraliakram1@gmail.com>
2021-11-23 16:03:19 +0500

    ADD/EDIT ADDED

--- src/app/shared/components/lw-select/lw-select.component.ts
+++ src/app/shared/components/lw-select/lw-select.component.ts
@@ -1,8 +1,9 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { AbstractControl, FormControl, FormGroup } from '@angular/forms';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { AbstractControl, FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
-  selector: 'app-lw-select',
+  selector: 'lw-select',
   templateUrl: './lw-select.component.html',
   styleUrls: ['./lw-select.component.scss']
 })
@@ -12,13 +13,42 @@ export class LwSelectComponent implements OnInit {
   @Input() type!: string;
   @Input() index!: number;
   @Input() form!: FormGroup;
-  constructor() { }
-
+  @Input() bindLabel?: string;
+  @Input() bindValue?: string;
+  @ViewChild('lwSELECT') lwSELECT!: ElementRef;
+  optionForm!: FormGroup;
+  modalType!: string;
+  constructor(private fb: FormBuilder, private modalService: NgbModal) { }
   ngOnInit(): void {
+    this.optionForm = this.fb.group({
+      value: [this.options.length - 1],
+      label: [null]
+    })
   }
 
-  addNewOption() {
-    this.options.push('Hello')
+  openModel(type: any, content: any) {
+    this.modalType = type;
+    if (type == 'edit') {
+      this.optionForm.patchValue({
+        value: this.lwSELECT.nativeElement.value,
+        label: !this.bindLabel ? this.options.filter((opt, i) => i == this.lwSELECT.nativeElement.value - 1)[0] : this.options.filter((opt, i) => i == this.lwSELECT.nativeElement.value - 1)[0][this.bindLabel]
+      }) 
+    } else {
+      this.optionForm.reset()
+    }
+    this.modalService.open(content);
   }
-
+  addNewOption(modal?: any) {
+    let newOption: any;
+    if (this.bindLabel && this.bindValue) {
+      newOption[this.bindValue] = this.optionForm.value.value;
+      newOption[this.bindLabel] = this.optionForm.value.label;
+    } else {
+      newOption = this.optionForm.value.label
+    }
+    this.modalType == 'add' ? this.options.push(newOption) :
+    this.options[this.lwSELECT.nativeElement.value - 1] = newOption;
+    this.modalType == 'add' && (<FormGroup>(<FormArray>this.form.controls[this.type !== 'thirdParty' ? this.type + 's' : 'thirdParties']).controls[this.index]).controls[this.control].setValue(this.options.length)
+    modal.dismiss();
+  }
 }
